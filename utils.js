5e6d7613b33bdc98a623662dea73b63dea2479a5
abhishekkrksinha-cloud <abhishek.kr.ksinha@gmail.com>
2026-04-16 12:23:14 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,21 @@
+export async function autoScroll(page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve) => {
+      let totalHeight = 0;
+      const distance = 300;
+
+      const timer = setInterval(() => {
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if (totalHeight >= document.body.scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 200);
+    });
+  });
+}
 export async function extractSignals(page) {
   return await page.evaluate(() => {
 
